0536e772b6e2834b45530012183d065bd9f6c621
kieran <kieran.boudouin@epitech.eu>
2024-02-21 21:41:57 +0100

    ERROR HANDLER

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -7,13 +7,14 @@ const sites = ref([]);
 onMounted(async () => {
   const response = await axios.get('http://fibre.larocheposay-vacances.com:3000/sites/');
   sites.value = response.data;
+  console.log(sites.value);
 });
 
 </script>
 
 <template>
   <div class="flex items-center justify-center min-h-screen">
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 justify-items-center justify-content-center">
+    <div  v-if="sites.length > 0" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 justify-items-center justify-content-center">
       <div v-for="site in sites" :key="site.site">
         <router-link :to='"/camping/"+site.site'>
           <div class="btn bg-base-100 shadow-xl text-2xl w-64 h-64 flex items-center justify-center">
@@ -21,6 +22,9 @@ onMounted(async () => {
           </div>
         </router-link>
       </div>
+    </div>
+    <div v-else class="text-error text-3xl">
+        Ooops, aucun site trouvé
     </div>
   </div>
 </template>
